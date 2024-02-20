49b00eecc0afa022ca14837da1463021fdc31d92
akiko-luka <ak.luka051@gmail.com>
2024-02-20 12:18:52 +0100

    add tech & accessories in navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,31 +5,35 @@ import { SidebarContext } from "../../contexts/SidebarContext.jsx";
 import { Link, NavLink } from "react-router-dom";
 
 // Styles
-import "./Header.css"
+import "./Header.css";
 
 // Icons
 import { BsCart4 } from "react-icons/bs";
 
 const Header = () => {
-    const { isOpen, setIsOpen } = useContext(SidebarContext);
-    return (
-        <div className="header">
-            <Link to={"/"}><h1>AKPICA.</h1></Link>
+  const { isOpen, setIsOpen } = useContext(SidebarContext);
+  return (
+    <div className="header">
+      <Link to={"/"}>
+        <h1>AKPICA.</h1>
+      </Link>
 
-            <nav className="header-nav">
-                <NavLink to={"/women"}>Women</NavLink>
-                <NavLink to={"/men"}>Men</NavLink>
-                <NavLink to={"/home-deco"}>Deco</NavLink>
-                <NavLink to={"/sunglasses"}>Sunglasses</NavLink>
-                <NavLink to={"/beauty"}>Beauty</NavLink>
-            </nav>
+      <nav className="header-nav">
+        <NavLink to={"/women"}>Women</NavLink>
+        <NavLink to={"/men"}>Men</NavLink>
+        <NavLink to={"/tech"}>Tech</NavLink>
+        <NavLink to={"/home-deco"}>Deco</NavLink>
+        <NavLink to={"/beauty"}>Beauty</NavLink>
+        <NavLink to={"/sunglasses"}>Sunglasses</NavLink>
+        <NavLink to={"/accessories"}>Accessories</NavLink>
+      </nav>
 
-            <div className="header-cart" onClick={() => setIsOpen(!isOpen)}>
-                <BsCart4 />
-                <span className="header-cart-span">2</span>
-            </div>
-        </div>
-    );
+      <div className="header-cart" onClick={() => setIsOpen(!isOpen)}>
+        <BsCart4 />
+        <span className="header-cart-span">2</span>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
