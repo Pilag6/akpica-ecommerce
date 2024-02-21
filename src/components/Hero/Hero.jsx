6a50147c68ea4c16dc30cd74b509a18bbf19b9d2
akiko-luka <ak.luka051@gmail.com>
2024-02-21 09:57:41 +0100

    hero new update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,11 +4,11 @@ import "./Hero.css";
 
 //icons
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { GoDotFill } from "react-icons/go";
 
-//images
+//images import
 import banner1 from "../../assets/banner-1.jpg";
 import banner2 from "../../assets/banner-2.jpg";
-
 
 const Hero = () => {
   //array of image-slider
@@ -16,12 +16,11 @@ const Hero = () => {
 
   //array of texts
   const textSlider = [
-    {promo: "New Collection", title: "Women Fashion"},
-    {promo: "New Collection", title: "Men Fashion"}
-  ]
+    { promo: "New Collection", title: "Women Fashion" },
+    { promo: "New Collection", title: "Men Fashion" },
+  ];
 
   const [slide, setSlide] = useState(0);
-  // const [slideText, setSlideText] = useState(0)
 
   const handlePrevSlide = () => {
     setSlide((prevSlide) =>
@@ -35,23 +34,34 @@ const Hero = () => {
     );
   };
 
+  const handleDot = (index) => {
+    setSlide(index)
+  };
+
   return (
     <>
       <div className="hero">
-
         <img src={imageSlider[slide]} alt={`image ${slide + 1}`} />
 
         <div className="btn-wrapper">
-          <button onClick={handlePrevSlide}><IoIosArrowBack /></button>
-          <button onClick={handleNextSlide}><IoIosArrowForward /></button>
+          <button className="left-btn" onClick={handleNextSlide}><IoIosArrowForward /></button>
+          <button className="right-btn" onClick={handlePrevSlide}><IoIosArrowBack /></button>
         </div>
 
         <div className="hero-text">
           <p>{textSlider[slide].promo}</p>
           <h1>{textSlider[slide].title}</h1>
-          <Link className="shop-now">SHOP NOW</Link>
-        </div>
+          {/* <Link className="shop-now">SHOP NOW</Link> */}
 
+          {/* dot-slider */}
+          <div className="dot-container">
+            {imageSlider.map((slide, index) => (
+              <div className="dot-btn" key={index} onClick={() => handleDot(index)}>
+                <GoDotFill />
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
