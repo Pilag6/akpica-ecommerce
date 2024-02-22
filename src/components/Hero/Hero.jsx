ce3fa357bc17196bd5a0dca42cf12927b13df024
Pilag6 <pilag6@gmail.com>
2024-02-22 19:03:48 +0100

    Update Hero component styles and add image slider

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,11 +11,11 @@ import banner2 from "../../assets/banner-2.jpg";
 import banner3 from "../../assets/banner-3.jpg";
 import banner4 from "../../assets/banner-4.jpg";
 import banner5 from "../../assets/banner-5.jpg";
-
+import { Link } from "react-router-dom";
 
 const Hero = () => {
     //array of image-slider
-    const imageSlider = [banner1, banner2, banner3, banner4, banner5,];
+    const imageSlider = [banner1, banner2, banner3, banner4, banner5];
 
     //array of texts
     const textSlider = [
@@ -23,8 +23,7 @@ const Hero = () => {
         { promo: "New Collection", title: "Elite Elegance" },
         { promo: "Our Gadgets", title: "Only Tech" },
         { promo: "Our Watches", title: "Time to Be" },
-        { promo: "The Future is Today", title: "New Reallity" },
-      
+        { promo: "The Future is Today", title: "New Reality" },
     ];
 
     const [slide, setSlide] = useState(0);
@@ -60,8 +59,33 @@ const Hero = () => {
                 </div>
 
                 <div className="hero-text">
-                    <p>{textSlider[slide].promo}</p>
-                    <h1>{textSlider[slide].title}</h1>
+                    <p className="animate__bounceInLeft">
+                        {textSlider[slide].promo}
+                    </p>
+                    <h2 className="animate__bounceInLeft">
+                        {textSlider[slide].title}
+                    </h2>
+                    {textSlider[slide].title === "Elite Elegance" ? (
+                        <Link to={"/men"} className="animate__bounceInLeft">
+                            Shop Now
+                        </Link>
+                    ) : textSlider[slide].title === "Blossom in Style" ? (
+                        <Link to={"/women"} className="animate__bounceInLeft">
+                            Shop Now
+                        </Link>
+                    ) : textSlider[slide].title === "Only Tech" ? (
+                        <Link to={"/tech"} className="animate__bounceInLeft">
+                            Shop Now
+                        </Link>
+                    ) : textSlider[slide].title === "Time to Be" ? (
+                        <Link to={"/men"} className="animate__bounceInLeft">
+                            Shop Now
+                        </Link>
+                    ) : textSlider[slide].title === "New Reality" ? (
+                        <Link to={"/tech"} className="animate__bounceInLeft">
+                            Shop Now
+                        </Link>
+                    ) : null}
                 </div>
 
                 {/* dot-slider */}
