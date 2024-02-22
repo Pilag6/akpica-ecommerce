f8bb1b9751630e5f4b2a47233d8fd31579efd8b6
Pilag6 <pilag6@gmail.com>
2024-02-22 13:30:47 +0100

    Update Banner1 component and add Banner1Card component

--- src/components/Banner1/Banner1.jsx
+++ src/components/Banner1/Banner1.jsx
@@ -1,55 +1,19 @@
 import "./Banner1.css";
 
 import photo1 from "../../assets/photo1.jpg";
-import { Link } from "react-router-dom";
+import photo2 from "../../assets/photo2.jpg";
+import photo3 from "../../assets/photo3.jpg";
+
+import Banner1Card from "./Banner1Card.jsx";
 
 const Banner1 = () => {
     return (
         <section className="banner-1">
-            <article>
-                <div className="banner-1-img">
-                    <img
-                        src={photo1}
-                        alt=""
-                    />
-                </div>
-                <div className="banner-1-content">
-                    <h4>SUNGLASSES</h4>
-                    <h3>SALE UP TO 50% OFF</h3>
-                    <Link to={"/sunglasses"}>SHOP NOW</Link>
-                    <hr />
-                </div>
-            </article>
 
-            <article>
-                <div className="banner-1-img">
-                    <img
-                        src="https://korando-demo.myshopify.com/cdn/shop/files/banner-style2_2.jpg?v=1613746893"
-                        alt=""
-                    />
-                </div>
-                <div className="banner-1-content">
-                    <h4>DSLR CAMERAS</h4>
-                    <h3>SALE UP TO 30% OFF</h3>
-                    <a href="">SHOP NOW</a>
-                    <hr />
-                </div>
-            </article>
-
-            <article>
-                <div className="banner-1-img">
-                    <img
-                        src="https://korando-demo.myshopify.com/cdn/shop/files/banner-style2_3.jpg?v=1613746893"
-                        alt=""
-                    />
-                </div>
-                <div className="banner-1-content">
-                    <h4>WOMEN&aposS FASHION</h4>
-                    <h3>SALE UP TO 40% OFF</h3>
-                    <a href="">SHOP NOW</a>
-                    <hr />
-                </div>
-            </article>
+            <Banner1Card image={photo1} title={"SUNGLASSES"} sale={"SALE UP TO 50% OFF"}/>
+            <Banner1Card image={photo2} title={"SMARTPHONES"} sale={"SALE UP TO 30% OFF"}/>
+            <Banner1Card image={photo3} title={"HOME DECO"} sale={"SALE UP TO 20% OFF"}/>
+            
         </section>
     );
 };
---
+++ src/components/Banner1/Banner1Card.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+
+const Banner1Card = ({ image, title, sale }) => {
+    return (
+        <article>
+            <div className="banner-1-img">
+                <img src={image} alt="" />
+            </div>
+            <div className="banner-1-content">
+                <h4>{title}</h4>
+                <h3>{sale}</h3>
+                <Link to={"/sunglasses"}>SHOP NOW</Link>
+                <hr />
+            </div>
+        </article>
+    );
+};
+
+export default Banner1Card;
